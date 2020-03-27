9d7bac8ccda5c72943931e7e90963d96c44bb647
Igor <Mascontris@yahoo.com>
2020-03-27 10:35:42 -0600

    fixed user slides

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
+
 //export default 'http://localhost:8000/'
 //export default 'https://capstone-merits.herokuapp.com/'
 export let googleApi = 'https://www.googleapis.com/books/v1/volumes?q=subject:"science fiction"&country=US&maxResults=5&startIndex='
